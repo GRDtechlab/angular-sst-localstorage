9f5995c9866d865dd645cf08bf136462f5257ac1
GaurangDhorda <dg.dhorda@gmail.com>
2020-02-07 12:40:59 +0530

    angular Universal SetUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular9';
+  isBrowser: boolean;
+  constructor(@Inject(PLATFORM_ID) private platFormId : Object){}
+  ngOnInit() {
+   this.isBrowser =  isPlatformBrowser(this.platFormId);
+   if(this.isBrowser){
+    console.log(Document);
+    this.getLocation();
+   }else{
+     console.log('server side')
+   }
+  }
+  getLocation(): void{
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          const longitude = position.coords.longitude;
+          const latitude = position.coords.latitude;
+        //  this.callApi(longitude, latitude);
+        console.log(`long is : ${longitude} with lat is : ${latitude}`);
+        });
+    } else {
+       console.log("No support for geolocation")
+    }
+  }
+
 }
